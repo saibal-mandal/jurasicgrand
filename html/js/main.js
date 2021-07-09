c4d3e8a5f1f49e44795ee5d672a1c094bc90869e
saibal-mandal <saibal.freelancer@gmail.com>
2021-07-10 00:34:19 +0530

    uploaded

---
+++ html/js/main.js
@@ -0,0 +1,111 @@
+//loader
+$(window).load(function() {
+    $('.loader').delay(400).fadeOut(200);
+	});	
+// ------------------------------------------
+/*==stiky menu js==*/
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 400) {
+        $("header").addClass("darkHeader");
+    } else {
+        $("header").removeClass("darkHeader");
+    }
+});
+// ---------------------------------------------
+//scroll to top	
+$(window).scroll(function(){
+	if($(this).scrollTop()>250){
+		$('.scrollup').fadeIn();
+	}else{
+		$('.scrollup').fadeOut();
+		}
+	});
+$('.scrollup').click(function(){
+	$("html, body").animate({scrollTop:0}, 600);
+	return false;
+	});	
+// ----------------------------------------------
+// header-section...
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar-nav');
+
+menu.addEventListener('click', function(){
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+});
+
+/*dropdown js*/
+const $dropdown = $(".dropdown");
+const $dropdownToggle = $(".dropdown-toggle");
+const $dropdownMenu = $(".dropdown-menu");
+const showClass = "show";
+ 
+$(window).on("load resize", function() {
+  if (this.matchMedia("(min-width: 992px)").matches) {
+    $dropdown.hover(
+      function() {
+        const $this = $(this);
+        $this.addClass(showClass);
+        $this.find($dropdownToggle).attr("aria-expanded", "true");
+        $this.find($dropdownMenu).addClass(showClass);
+      },
+      function() {
+        const $this = $(this);
+        $this.removeClass(showClass);
+        $this.find($dropdownToggle).attr("aria-expanded", "false");
+        $this.find($dropdownMenu).removeClass(showClass);
+      }
+    );
+  } else {
+    $dropdown.off("mouseenter mouseleave");
+  }
+});
+
+ // owl-carousel banner 
+$('#banner-slider').owlCarousel({
+        loop: true,
+        nav: false,
+        autoplay: true,
+        autoplayTimeout: 4500,
+        smartSpeed: 2550,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    });
+
+//light box light box light box
+            $('.fancybox').fancybox();
+
+            $('.fancybox-buttons').fancybox({
+                openEffect  : 'none',
+                closeEffect : 'none',
+
+                prevEffect : 'none',
+                nextEffect : 'none',
+
+                closeBtn  : false,
+
+                helpers : {
+                    title : {
+                        type : 'inside'
+                    },
+                    buttons : {}
+                },
+
+                afterLoad : function() {
+                    this.title = 'Jurasicgard ' + (this.index + 1) + ' of ' + this.group.length + (this.title ? ' - ' + this.title : '');
+                }
+            });
+
